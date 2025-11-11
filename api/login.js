2ec3ed194277f8d2bcb86104553c7e0c5edf9f03
Oliur292007 <136627484+Oliur292007@users.noreply.github.com>
2025-11-11 09:30:32 +0600

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://wfhpqqszntpvucmgpmds.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default async function handler(req, res) {
   if(req.method !== 'POST') return res.status(405).json({ message: 'Method not allowed' });
